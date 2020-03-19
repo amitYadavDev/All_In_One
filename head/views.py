d3cee4caee369d2bba700fd00cf50c0f3ec0d7d9
Amit Yadav <amit8955471017@gmail.com>
2020-03-19 15:07:57 +0530

    updated

--- head/views.py
+++ head/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#Home page
+def home(request):
+    return render(request, 'home.html')
+
+
+
+#login page
 def login(request):
-    return render(request, 'log.html')
+    return render(request, 'login.html')
 
 # Create your views here.
